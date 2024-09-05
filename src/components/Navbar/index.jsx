001b4c3fa491aabd35a2535005b44790d055b012
owenrud <owenrudiantoso@gmail.com>
2024-09-05 15:45:40 +0800

    fix bug

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,10 @@
 "use client"
 import Link from "next/link";
 import { Navbar,Flowbite } from "flowbite-react";
+import { useSelectedLayoutSegment } from 'next/navigation'
 
 const NavbarComponent = () =>{
+  const segment = useSelectedLayoutSegment()
     const customTheme = {
         navbar: {
           "root": {
@@ -15,11 +17,19 @@ const NavbarComponent = () =>{
         "link": {
     "base": "block py-2 pl-3 pr-4 md:p-0 text-lg",
     "active": {
-      "on": " text-color-primary md:text-color-primary font-bold",
-      "off": "text-color-secondary hover:bg-color-secondary hover:text-color-primary md:border-0 hover:bg-opacity-5  md:hover:bg-transparent  md:dark:hover:bg-transparent hover:font-bold"
+      "on": " text-color-primary font-bold",
+      "off": "text-color-secondary hover:bg-color-primary md:hover:bg-transparent hover:text-color-primary md:border-0  hover:font-bold"
     },
     "disabled": {
       "on": "text-gray-400 hover:cursor-not-allowed dark:text-gray-600",
+      "off": ""
+    }
+  },
+  "collapse": {
+    "base": "w-full md:block md:w-auto",
+    "list": "mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-8 md:text-sm md:font-medium",
+    "hidden": {
+      "on": "hidden",
       "off": ""
     }
   },
@@ -31,22 +41,22 @@ const NavbarComponent = () =>{
     };
     return(
         <Flowbite theme={{ theme: customTheme }}>
-        <Navbar>
+        <Navbar className="bg-color-white" theme={customTheme}>
         <Navbar.Brand as={Link} href="/">
           <img src="./favicon.ico" className="mr-3 h-6 sm:h-9" alt=" Logo" />
           <span className="self-center whitespace-nowrap text-lg md:text-3xl font-bold">Kabuki Autoshop Samarinda</span>
         </Navbar.Brand>
         <Navbar.Toggle />
-        <Navbar.Collapse>
-          <Navbar.Link href="#" active>
+        <Navbar.Collapse theme={customTheme}>
+          <Navbar.Link as ={Link} className={segment == null ? "text-color-primary font-bold bg-color-primary bg-opacity-10 ":"text-color-secondary hover:bg-color-primary md:hover:bg-transparent hover:text-color-primary md:border-0  hover:font-bold"} theme={customTheme} href="/" > 
             Home
           </Navbar.Link>
-          <Navbar.Link as={Link} href="#">
+          <Navbar.Link as={Link} className="hover:text-color-primary hover:bg-color-primary hover:bg-opacity-10" href="#">
             About
           </Navbar.Link>
-          <Navbar.Link href="#">Services</Navbar.Link>
-          <Navbar.Link href="#">Pricing</Navbar.Link>
-          <Navbar.Link href="#">Contact</Navbar.Link>
+          <Navbar.Link as={Link} className="hover:bg-color-primary hover:bg-opacity-10" href="#">Services</Navbar.Link>
+          <Navbar.Link as={Link} className="hover:bg-color-primary hover:bg-opacity-10" href="#">Pricing</Navbar.Link>
+          <Navbar.Link as={Link} className="hover:bg-color-primary hover:bg-opacity-10" href="#">Contact</Navbar.Link>
         </Navbar.Collapse>
       </Navbar>
       </Flowbite>
